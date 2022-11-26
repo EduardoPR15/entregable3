edc2faf66309e091f491c08fd185a26d543a370e
EduardoPR15 <lpeduardo15@gmail.com>
2022-11-25 22:16:39 -0600

    final

--- src/users/users.services.js
+++ src/users/users.services.js
@@ -1,3 +1,4 @@
+const Users = require('../models/users.models')
 const usersControllers = require('./users.controllers')
 
 
@@ -12,20 +13,59 @@ const getAllUsers = (req,res) => {
 
 }
 
-const getUserById = () => {
-    
+const getUserById = (req,res) => {
+    const id = req.params.id
+    usersControllers.findUserById(id)
+
+    .then( data => {
+        if(data){
+         res.status(200).json(data)   
+        } else{
+            res.status(404).json({message: 'invalid ID'})
+        }
+        
+    })
+    .catch(err =>{
+        res.status(400).json({message: err.message})
+    })
 }
 
-const postUser = () => {
-    
+const postUser = (req,res) => {
+    const { first_name, last_name, email, password, birthday } = req.body
+
+    usersControllers.createUser({ first_name, last_name, email, password, birthday })
+    .then( data => {
+        res.status(201).json(data)   
+    })
+    .catch(err =>{
+        res.status(400).json({message: err.message})
+    })
 }
 
-const patchUser = () => {
-    
+const patchUser = (req,res) => {
+    const id = req.params.id
+    const { first_name, last_name, email, password, birthday } = req.body
+    usersControllers.updateUser(id,{first_name, last_name, email, password, birthday })
+
+    .then( data => {
+        if(data){
+        res.status(200).json({message: 'user updated at' + id})   
+        }else{
+            res.status(404).json({message: 'invalid  id'})
+
+        }
+
+    })
+    .catch(err =>{
+        res.status(400).json({message: err.message})
+    })
+
+
 }
 
-const deleteUser = () => {
-    
+const deleteUser = (req,res) => {
+    const id = req.params.id
+    usersControllers.deleteUser()
 }
 
 
